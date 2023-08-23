fcc3ac7253112fa9c5ae27c383b12c049b2d81f3
Marion Robert <mrobert148@gmail.com>
2023-08-23 12:28:28 +0200

    comment card ok

--- src/components/activity-card.jsx
+++ src/components/activity-card.jsx
@@ -24,7 +24,7 @@ const ActivityCard = (props) => {
       { activity !== null && <article className="activity-card">
         { activity.urlPicture !== null ? <img src={activity.urlPicture}/> : <img src={`${config.pict_url}/no-pict.jpg`} /> }
         <h3>{activity.title}</h3>
-        <p>{activity.description.substring(0, 80)}...</p>
+        <p>{activity.description.substring(0, 70)}...</p>
         <p className="activity-card-points">{activity.points} pts</p>
         { author !== null && author.avatar !== null ? <img src={author.avatar} className="activity-card-avatar"/> : <img src={`${config.pict_url}/user.png`} className="activity-card-avatar"/> }
         <button className="activity-card-btn"><Link to={`/activity/details/${activity.id}`}> Je découvre</Link></button>
--- src/components/comment-card.jsx
+++ src/components/comment-card.jsx
@@ -2,7 +2,8 @@ import { config } from "../config"
 import { getOneUserById } from "../api/user"
 import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faStar} from "@fortawesome/free-solid-svg-icons"
+import { faStar as fullStar} from "@fortawesome/free-solid-svg-icons"
+import { faStar } from "@fortawesome/free-regular-svg-icons"
 
 const CommentCard = (props) => {
   const comment = props.comment
@@ -22,11 +23,22 @@ const CommentCard = (props) => {
 
   return (
     <>
-      { comment !== null && <article className="comment-card">
-        <h3>{comment.title}</h3>
-        <p>{comment.content.substring(0, 50)}...</p>
-        <p>{comment.score} <FontAwesomeIcon icon={faStar}/></p>
-        { author !== null && author.avatar !== null ? <img src={author.avatar} className="activity-card-avatar"/> : <img src={`${config.pict_url}/user.png`} className="activity-card-avatar"/> }
+      { comment !== null && author !== null && <article className="comment-card">
+        { author.avatar !== null ? <img src={author.avatar} className="comment-card-avatar"/> : <img src={`${config.pict_url}/user.png`} className="comment-card-avatar"/> }
+
+        <div className="comment-card-data">
+          <h3 className="comment-card-username" >{author.firstName} {author.lastName.substring(0, 1).toUpperCase()}.</h3>
+          <p className="comment-card-score">
+            <FontAwesomeIcon icon={fullStar}/>
+            <FontAwesomeIcon icon={fullStar}/>
+            <FontAwesomeIcon icon={fullStar}/>
+            <FontAwesomeIcon icon={fullStar}/>
+            { comment.score === 5 ? <FontAwesomeIcon icon={fullStar}/> : <FontAwesomeIcon icon={faStar}/> } {comment.score}/5
+          </p>
+        </div>
+
+        <h3 className="comment-card-title">{comment.title}</h3>
+        <p>« {comment.content} »</p>
       </article>
       }
     </>
