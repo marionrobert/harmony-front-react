9da0c1122828217e66c0d2d609b956d8f5a8087c
Marion Robert <mrobert148@gmail.com>
2023-08-26 10:25:18 +0200

    all productspage ok simple design for now

---
+++ src/containers/activities.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { selectActivities } from "../slices/activitySlice";
+import { selectBasket, updateBasket } from "../slices/basketSlice";
+import ActivityCard from "../components/activity-card"
+
+const Activities = () => {
+  const activities = useSelector(selectActivities)
+
+  return (
+    <>
+      { activities.activities.length > 0 &&
+        <section className="home-activities">
+          <h1>Toutes les activités disponibles</h1>
+          {activities.activities.slice(-10).map(activity => {
+            return <ActivityCard key={activity.id} activity={activity} />
+          })}
+        </section>
+      }
+    </>
+  )
+}
+
+export default Activities;
